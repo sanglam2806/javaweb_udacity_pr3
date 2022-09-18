c2c4ab06adf977d4e8deec4a209bad29b410e03c
Tri Vo <tri.vo@workforceoptimizer.com>
2022-09-18 22:41:32 +0700

    saving schedule

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
@@ -1,13 +1,73 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import com.udacity.jdnd.course3.critter.user.employee.Employee;
 
-@Embeddable
+@Entity
 public class Schedule implements Serializable {
-    private long emplyeeId;
-    private long petId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @OneToMany(fetch = FetchType.LAZY, targetEntity=Employee.class)
+    private List<Employee> employees;
+
+    @OneToMany(fetch = FetchType.LAZY, targetEntity=Pet.class)
+    private List<Pet> pets;
+    private LocalDate date;
+//    private Set<EmployeeSkill> activities;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+//    public Set<EmployeeSkill> getActivities() {
+//        return activities;
+//    }
+//
+//    public void setActivities(Set<EmployeeSkill> activities) {
+//        this.activities = activities;
+//    }
 }
